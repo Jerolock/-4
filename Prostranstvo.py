9e9b7477c9e5652e801e34373d352fd9beb07f0a
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-04-30 09:09:52 +0500

    Update Prostranstvo.py

--- Prostranstvo.py
+++ Prostranstvo.py
@@ -1,10 +1,9 @@
 def test_function():
-    print('Я функция', inner_function())
+    print('Я функция')
 
-
-def inner_function():
-    print('Я в области видимости функции test_function')
+    def inner_function():
+        print('Я в области видимости функции test_function')
 
 
 test_function()
-inner_function()
+# inner_function() не вызывается и пишется что мы имели в виду test_function
