a6c9722ae96806c637bd385736a65dcf13536fe8
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-05-11 13:18:26 +0500

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -1,8 +1,13 @@
 class Building:
     def __init__(self):
         self.numberOfFloors = 15
-        self.buildingType = 'Небоскреб'
+        self.buildingType = 'дом'
 
     def __eq__(self, other):
-        return self.numberOfFloors == other.numberOfFloors
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
+
+b1 = Building()
+b2 = Building()
+if Building.__eq__(self=b1, other=b2):
+    print('О два дома')
